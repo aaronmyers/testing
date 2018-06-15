cc9b4f1be5c3114d494efc1aeb50d0b02c1de486
Aaron <aaron.n.myers@gmail.com>
2018-06-15 01:06:01 -0500

    adding db query

--- app.py
+++ app.py
@@ -11,13 +11,17 @@ mongo = PyMongo(app)
 @app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    value = mongo.db.testColl.find_one()
 
     return """
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
 
+    <h2> Your query returned with {query}.</h2>
     <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
+
+
+    """.format(time=the_time,query=value)
 
 if __name__ == '__main__':
     app.run()
