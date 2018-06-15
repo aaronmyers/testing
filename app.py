5c59e4cc0352b6af34ebd46079d9b5d5ac348591
Aaron <aaron.n.myers@gmail.com>
2018-06-15 00:50:14 -0500

    updating application

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import os
 from datetime import datetime
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['MONGODB_URI']
 
 @app.route('/')
 def homepage():
