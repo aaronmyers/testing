2e4b0b79f2796725e9d0c9b63c45a57fb84ac139
Aaron <aaron.n.myers@gmail.com>
2018-06-15 01:00:06 -0500

    updating mongodb connection

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
 import os
 from datetime import datetime
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['MONGODB_URI']
+mongo = PyMongo(app)
 
 @app.route('/')
 def homepage():
